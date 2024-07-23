2a3c8ffe9dc4d4fdc95d4477690cef74fd348690
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2024-07-23 18:52:00 +0900

    Initial commit

---
+++ Source/LastOfNN/LastOfNN.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LastOfNN.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LastOfNN, "LastOfNN" );
